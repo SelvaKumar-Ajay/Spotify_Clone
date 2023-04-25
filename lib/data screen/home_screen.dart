59f264ade75210e98623add0a5f69de7a918cb21
Selvakumar <skajay1622@gmail.com>
2023-04-25 16:56:29 +0530

    Recent viewed playlists added in home tab

--- lib/data screen/home_screen.dart
+++ lib/data screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/constants/constants.dart';
 import 'package:spotify/functions/functions.dart';
+import 'package:spotify/reusable_widgets/reusable_widgets.dart';
 // import 'package:spotify/reusable_widgets/reusable_widgets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,12 +16,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
-    final double imgBoxWidth = (width * 0.4) * 0.3;
-    final double nonImgBoxWidth = width - imgBoxWidth;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             dateTimeGreeting(),
@@ -33,26 +33,35 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(
             height: 20.0,
           ),
-          Container(
-            decoration: BoxDecoration(
-                color: whiteColor.withOpacity(0.4),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10.0)),
-            height: height * 0.1,
-            width: width * 0.4,
-            child: Row(children: [
-              // ignore: sized_box_for_whitespace
-              Container(
-                height: height * 0.1,
-                width: imgBoxWidth,
-                child: Image.asset(IconConstants.libraryIcon),
-              ),
-              SizedBox(
-                height: height * 0.1,
-                width: nonImgBoxWidth,
-                child: const Text(StringConstants.likedSongs),
-              ),
-            ]),
+          rowRecentPlayList(
+            height: height,
+            width: width,
+            firstImage: IconConstants.likeIcon,
+            firstText: StringConstants.likedSongs,
+            secondImage: IconConstants.likeIcon,
+            secondText: StringConstants.likedSongs,
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          rowRecentPlayList(
+            height: height,
+            width: width,
+            firstImage: IconConstants.microphoneIcon,
+            firstText: StringConstants.likedSongs,
+            secondImage: IconConstants.microphoneIcon,
+            secondText: StringConstants.goodMorning,
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          rowRecentPlayList(
+            height: height,
+            width: width,
+            firstImage: IconConstants.nextTrcakIcon,
+            firstText: StringConstants.spotifyClone,
+            secondImage: IconConstants.loopIcon,
+            secondText: StringConstants.search,
           ),
         ],
       ),
--- lib/reusable_widgets/reusable_widgets.dart
+++ lib/reusable_widgets/reusable_widgets.dart
@@ -11,3 +11,70 @@ BottomNavigationBarItem bottomNavigationBarItem(
           color: page == false ? greyColor.withOpacity(0.5) : whiteColor),
       label: label);
 }
+
+// Row of Recent Playlist
+Row rowRecentPlayList({
+  required double height,
+  required double width,
+  required String firstImage,
+  required String secondImage,
+  required String firstText,
+  required String secondText,
+}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      recentViewedPlaylist(
+          height: height, width: width, img: firstImage, text: firstText),
+      SizedBox(
+        width: ((width * 0.4) * 0.1) * 0.5,
+      ),
+      recentViewedPlaylist(
+          height: height, width: width, img: secondImage, text: secondText)
+    ],
+  );
+}
+
+//Recently viewed Playlist Design
+Container recentViewedPlaylist({
+  required height,
+  required width,
+  required String img,
+  required String text,
+}) {
+  final double imgBoxWidth = (width * 0.4) * 0.4;
+  final double nonImgBoxWidth = (width * 0.4) * 0.7;
+  return Container(
+    decoration: BoxDecoration(
+        color: searchBarColor.withOpacity(0.4),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(8.0)),
+    clipBehavior: Clip.antiAlias,
+    height: height * 0.1,
+    width: ((width * 0.5) * 0.9) * 1.03,
+    child: Row(children: [
+      // ignore: sized_box_for_whitespace
+      Container(
+        height: height * 0.1,
+        width: imgBoxWidth,
+        child: Image.asset(img, fit: BoxFit.cover),
+      ),
+      SizedBox(width: (width * 0.2) * 0.1),
+      Expanded(
+        child: SizedBox(
+          height: height * 0.1,
+          width: nonImgBoxWidth,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(text,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w500, fontSize: 16.0),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                softWrap: false),
+          ),
+        ),
+      ),
+    ]),
+  );
+}
